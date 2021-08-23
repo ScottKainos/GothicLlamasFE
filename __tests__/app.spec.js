49685a2158ada2256f45a16a2121fcc0596324b2
Rees Pozzi <rees.pozzi@kainos.com>
2021-08-23 13:55:44 +0100

    Added basic testing infrastructure

---
+++ __tests__/app.spec.js
@@ -0,0 +1,11 @@
+describe("Routing setup", () => {
+  test("serviceStartedSuccessfully", () => {
+    //TODO
+
+    /*
+        Implement test to check service has started successfully
+        and expect value at the end.
+    */
+
+  });
+});
