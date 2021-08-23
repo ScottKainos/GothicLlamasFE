0b51af467f6637bd37de3e35f92a3ab439d7d868
Rees Pozzi <rees.pozzi@kainos.com>
2021-08-23 12:23:12 +0100

    Slight code refactor

--- app.js
+++ app.js
@@ -4,37 +4,26 @@ const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks');
 
 //'viewdir' tells nunjucks where to look for file templates
-
 nunjucks.configure('views', {
-
     express: app
-    
 });
 
 app.set('view engine', 'html');
 
 var middle = function (req, res, next) {
-
     console.log('middleware');
-
     //calls next function which may be another middleware, if not it calls original callback on url requested
-
     next();
-
 };
 
-//render the homepage when site accessed
-
+//render the homepage when root of site accessed
 app.get('/', function (req, res) {
-
     res.render('index');
-
 });
 
 app.use(middle)
 
+//start listening on 7999 port
 app.listen(7999, function() {
-
     console.log('Started')
-    
 });
