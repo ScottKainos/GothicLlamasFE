02acfc365bac9f7e25a19b595bc2fcca60ae25f4
Rees Pozzi <rees.pozzi@kainos.com>
2021-08-23 15:12:02 +0100

    Unit tests for app.js pushed

--- __tests__/app.spec.js
+++ __tests__/app.spec.js
@@ -1,11 +1,31 @@
-describe("Routing setup", () => {
-  test("serviceStartedSuccessfully", () => {
-    //TODO
+//mocking and setup
 
-    /*
-        Implement test to check service has started successfully
-        and expect value at the end.
-    */
+const mockApp = {
+    get: jest.fn(),
+    listen: jest.fn(),
+    set: jest.fn(),
+    use: jest.fn()
+}
 
-  });
+jest.mock('express', () => jest.fn(() => mockApp))
+require('../app.js')
+
+describe('Basic set up testing', () => {
+    test("job-roles Route serves job-roles html page", () => {
+        //call get function here?
+        expect(mockApp.get).toHaveBeenCalledWith('/job-roles', expect.any(Function))
+
+        //tracks all app.get calls when require('../app.js') line is run, get ('job-roles') is second hence [1][1] call
+        const behaviour = mockApp.get.mock.calls[1][1] // grab the second [1] param of the second [1] call
+        const res = { render: jest.fn() }
+        
+        //call function used by get handler
+        behaviour(null, res)
+
+        expect(res.render).toHaveBeenCalledWith('job-roles', {})
+    });
+
+    test('Start successfully and listen on port 7999', () => {
+        expect(mockApp.listen).toHaveBeenCalledWith(7999, expect.any(Function))
+    })
 });
