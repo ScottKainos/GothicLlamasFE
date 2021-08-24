fab0ff9248cc1bcb9361064237cee40fbfd7d3fe
Rees Pozzi <rees.pozzi@kainos.com>
2021-08-24 09:45:41 +0100

    Sending basic get request to future java API endpoint

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const nunjucks = require('nunjucks');
+const http = require('http');
+
+/**
+ * Only add new .get() methods below current last one
+ * so that npm test runs as expected
+ */
 
 //'viewdir' tells nunjucks where to look for file templates
 nunjucks.configure('views', {
@@ -21,12 +27,32 @@ app.get('/', function (req, res) {
     res.render('index');    
 });
 
+
 //render the job-roles page
 app.get('/job-roles', function (req, res) {
     res.render('job-roles', {});
 });
 
 app.use(middle)
+
+app.get('/testJava', function(req, res){
+    //TODO
+    const httpConfig = {
+        hostname: 'localhost',
+        port: 8080,
+        // Can set below attributes as and when they need to be used.
+        path: '/hello.html',
+        method: 'GET',
+        headers: {
+            'Content-Type': 'text/html',
+        }
+    };
+    
+    //http.request async, needs fix
+    const javaAPIReq = http.request(httpConfig)
+    javaAPIReq.end()
+    console.log(javaAPIReq)
+});
 
 //start listening on 7999 port
 app.listen(7999, function() {
