fd147f4149c0d6a947c1be28a8c24d4a96230fc4
Sylwia Luczak-Jagiela <sylwia.luczak-jagiela@kainos.com>
2021-08-30 15:37:45 +0200

    add dependency for applitools

--- selenium_test/src/main/java/us_selenium_test/framework/FunctionalTest.java
+++ selenium_test/src/main/java/us_selenium_test/framework/FunctionalTest.java
@@ -1,16 +1,12 @@
-package us001_selenium_test.framework;
+package us_selenium_test.framework;
 
-import com.testautomationguru.ocular.Ocular;
 import io.github.bonigarcia.wdm.WebDriverManager;
-
 import org.junit.After;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.BeforeSuite;
 
-import java.nio.file.Paths;
 import java.util.concurrent.TimeUnit;
 
 import static org.openqa.selenium.remote.CapabilityType.BROWSER_VERSION;
@@ -25,17 +21,6 @@ public class FunctionalTest {
         WebDriverManager.chromedriver().browserVersion(browserVersion).setup();
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @BeforeSuite
-    public static void ocularSetUp (){
-        String resultPath = "/Users/sylwia.luczak-jagiela/IdeaProjects/GothicLlamasFE/selenium_test/src/main/java/us001_selenium_test/page_models/ocular_result";
-        String snapshotPath = "/Users/sylwia.luczak-jagiela/IdeaProjects/GothicLlamasFE/selenium_test/src/main/java/us001_selenium_test/ocular_snpshot";
-        Ocular.config()
-                .resultPath(Paths.get(resultPath))
-                .snapshotPath(Paths.get(snapshotPath))
-                .globalSimilarity(95)
-                .saveSnapshot(true);
     }
 
     @After
--- selenium_test/src/test/java/US001_test.java
+++ selenium_test/src/test/java/US001_test.java
@@ -2,12 +2,12 @@ import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import us001_selenium_test.framework.FunctionalTest;
+import us_selenium_test.framework.FunctionalTest;
 
 public class US001_test extends FunctionalTest {
 
     private String indexPageUrl = "http://localhost:7999";
-    private String jobPageUrl = "http://localhost:7999/job-roles";
+    private String jobRolePageUrl = "http://localhost:7999/job-roles";
 
     @Test
     public void isJobRolePageExist(){
@@ -15,10 +15,13 @@ public class US001_test extends FunctionalTest {
         WebElement jobRoleLink = driver.findElement(By.linkText("Job Roles"));
         jobRoleLink.click();
         String URL = driver.getCurrentUrl();
-        Assert.assertEquals(jobPageUrl, URL );
+        Assert.assertEquals(jobRolePageUrl, URL );
         System.out.println(URL);
     }
 
+    @Test
+    public void isJobRolePageLooksLikeShould(){
 
+    }
 
 }
