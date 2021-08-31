8213235f9632ee62d52d9d0e8a23dc534bdad707
Sylwia Luczak-Jagiela <sylwia.luczak-jagiela@kainos.com>
2021-08-31 11:39:33 +0200

    add test for us002

--- selenium_test/src/test/java/US001_test.java
+++ selenium_test/src/test/java/US001_test.java
@@ -6,13 +6,12 @@ import us_selenium_test.framework.FunctionalTest;
 
 public class US001_test extends FunctionalTest {
 
-    private String indexPageUrl = "http://localhost:7999";
-    private String jobRolePageUrl = "http://localhost:7999/job-roles";
-
     @Test
     public void isJobRolePageExist_TC01(){
+        String indexPageUrl = "http://localhost:7999/";
+        String jobRolePageUrl = "http://localhost:7999/job-roles";
         driver.get(indexPageUrl);
-        WebElement jobRoleLink = driver.findElement(By.linkText("Job Roles"));
+        WebElement jobRoleLink = driver.findElement(By.cssSelector("body:nth-child(2) div.header:nth-child(1) div.inline:nth-child(2) > a:nth-child(1)"));
         jobRoleLink.click();
         String URL = driver.getCurrentUrl();
         Assert.assertEquals(jobRolePageUrl, URL );
