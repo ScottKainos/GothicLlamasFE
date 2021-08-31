d1607f7703aaa200d4d24c96dc8fac91eaa38112
Sylwia Luczak-Jagiela <sylwia.luczak-jagiela@kainos.com>
2021-08-31 15:19:17 +0200

    add test for US001 and US002 for chrome and firefox

--- selenium_test/src/test/java/firefox/US002_firefox.java
+++ selenium_test/src/test/java/firefox/US002_firefox.java
@@ -1,31 +1,32 @@
-package chrome;
+package firefox;
 
 import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
-import us_selenium_test.framework.FunctionalTest;
+import us_selenium_test.framework.FunctionalTestFirefox;
 
 import java.util.List;
 
-public class US002_test extends FunctionalTest {
+public class US002_firefox extends FunctionalTestFirefox {
+
     private String indexPageUrl = "http://localhost:7999";
     private String jobSpecPageUrl = "http://localhost:7999/job-spec";
 
     @Test
     public void isJobSpecPageExist_TC01() {
-        driver.get(indexPageUrl);
-        WebElement jobRoleLink = driver.findElement(By.linkText("Job Specification"));
+        firefoxDriver.get(indexPageUrl);
+        WebElement jobRoleLink = firefoxDriver.findElement(By.linkText("Job Specification"));
         jobRoleLink.click();
-        String URL = driver.getCurrentUrl();
+        String URL = firefoxDriver.getCurrentUrl();
         Assert.assertEquals(jobSpecPageUrl, URL);
         System.out.println(URL);
     }
 
     @Test
     public void isJobSpecPageLooksLikeItShould_TC02() {
-        driver.get(jobSpecPageUrl);
-        List<WebElement> listOfElements = driver.findElements(By.tagName("th"));
+        firefoxDriver.get(jobSpecPageUrl);
+        List<WebElement> listOfElements = firefoxDriver.findElements(By.tagName("th"));
         // Check if we got a list of 32 elements
         Assert.assertEquals(3, listOfElements.size());
         // Check if first column is "Job Role"
@@ -40,28 +41,29 @@ public class US002_test extends FunctionalTest {
             System.out.println("Column name: " + listOfElements.get(i).getText());
         }
         // Check how many rows are in the table
-        List<WebElement> listOfElementsOfTable = driver.findElements(By.tagName("td"));
-        int rowsInTable = listOfElementsOfTable.size() /3 ;
+        List<WebElement> listOfElementsOfTable = firefoxDriver.findElements(By.tagName("td"));
+        int rowsInTable = listOfElementsOfTable.size() / 3;
         System.out.printf("Found this many rows: " + rowsInTable);
     }
 
     @Test
     public void isJobSpecPageLinksAreCorrect_TC03() {
         // Chose first link in table
-        driver.get(jobSpecPageUrl);
-        WebElement linkFrom1RowInTable = driver.findElement(By.xpath("//body[1]/div[3]/table[1]/tbody[1]/tr[1]/td[3]/a[1]"));
+        firefoxDriver.get(jobSpecPageUrl);
+        WebElement linkFrom1RowInTable = firefoxDriver.findElement(By.xpath("//body[1]/div[3]/table[1]/tbody[1]/tr[1]/td[3]/a[1]"));
         linkFrom1RowInTable.click();
-        String URL1FromTable = driver.getCurrentUrl();
+        String URL1FromTable = firefoxDriver.getCurrentUrl();
         // Check if the URL has changed
         Assert.assertNotSame(jobSpecPageUrl, URL1FromTable);
 
         // Chose ten link in table
-        driver.get(jobSpecPageUrl);
-        WebElement linkFrom10RowInTable = driver.findElement(By.xpath("//body[1]/div[3]/table[1]/tbody[10]/tr[1]/td[3]/a[1]"));
+        firefoxDriver.get(jobSpecPageUrl);
+        WebElement linkFrom10RowInTable = firefoxDriver.findElement(By.xpath("//body[1]/div[3]/table[1]/tbody[10]/tr[1]/td[3]/a[1]"));
         linkFrom10RowInTable.click();
-        String URL10FromTable = driver.getCurrentUrl();
+        String URL10FromTable = firefoxDriver.getCurrentUrl();
         // Check if the URL has changed
         Assert.assertNotSame(jobSpecPageUrl, URL10FromTable);
     }
 
 }
+
